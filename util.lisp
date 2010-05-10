8d1eaf4594a93bcf3c66180a94578ab813e7e0a3
phjgt <phjgt@582adf2b-9e63-40c7-8b34-6b4bcd983156>
2010-05-10 23:15:47 +0000

    不要な関数を削除
    git-svn-id: http://svn.osdn.net/svnroot/creole@28 582adf2b-9e63-40c7-8b34-6b4bcd983156

--- util.lisp
+++ util.lisp
@@ -6,40 +6,11 @@
   `(let ((it ,exp))
      (if it ,then ,else)))
 
-(defmacro muffle-warn (&body body)
-  `(locally 
-    #+SBCL (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
-    ,@body))
-
-(defmacro each-char-code ((code string &key return (start 0) end) &body body)
-  (let ((i (gensym)))
-    `(do* ((,i ,start (1+ ,i))
-	   (,code #1=(char-code (char ,string ,i)) #1#))
-	  ((>= ,i ,(or end `(length ,string))) ,return)
-       (declare (array-index ,i))
-       ,@body)))
-
 (defmacro each-code ((code charseq &key return) &body body)
   `(charseq:each (#1=#:char ,charseq ,return)
      (let ((,code (char-code #1#)))
        ,@body)))
 
-(defun to-simple-characters (source start end)
-  (let ((dist (make-array (- end start) :element-type 'character)))
-    (loop FOR i FROM start BELOW end 
-	  FOR j FROM 0 DO
-      (setf (aref dist j) (muffle-warn (aref source i))))
-    dist))
-
-;; TODO: delete
-(defmacro ensure-simple-characters ((s start end) &body body)
-  `(multiple-value-bind (,s ,start ,end)
-     (etypecase ,s
-       (simple-characters (values ,s ,start ,end))
-       (string (values (to-simple-characters ,s ,start ,end) 0 (- ,end ,start))))
-     (declare (simple-characters ,s))
-     ,@body))
-  
 (defmacro defconst-onceonly (name value &optional doc)
   `(defconstant ,name (if (boundp ',name) (symbol-value ',name) ,value)
      ,@(when doc (list doc))))
